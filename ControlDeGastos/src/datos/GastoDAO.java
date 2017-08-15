cdcb162635b28b92eb8fc9bbe0ea12275194e5f6
Unknown <lalo_dd@hotmail.com>
2017-08-15 10:41:52 -0500

    GastoDAO funciones

--- ControlDeGastos/src/datos/GastoDAO.java
+++ ControlDeGastos/src/datos/GastoDAO.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package datos;
 
-/**
- *
- * @author lalo
- */
+import java.time.LocalDate;
+import javafx.collections.ObservableList;
+import logica.Gasto;
+
 public class GastoDAO {
+    
+    public ObservableList<Gasto> consultarGasto(LocalDate fecha);
+    public boolean agregarGasto(double Gasto, LocalDate fecha, String descripcion);
     
 }
