7fdd2cd522804d04168780f5eab28925d9e5b15a
Unknown <andresdglez@gmail.com>
2017-08-15 10:41:59 -0500

    Gasto clase

--- ControlDeGastos/src/logica/Gasto.java
+++ ControlDeGastos/src/logica/Gasto.java
@@ -5,10 +5,38 @@
  */
 package logica;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author lalo
  */
 public class Gasto {
-    
+    private LocalDate fecha;
+    private Double gasto;
+    private String descripcion;
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Double getGasto() {
+        return gasto;
+    }
+
+    public void setGasto(Double gasto) {
+        this.gasto = gasto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
