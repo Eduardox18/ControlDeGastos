93ee494fb80377b0fc5b8f639d5cab20c55124e3
Unknown <lalo_dd@hotmail.com>
2017-08-15 10:58:55 -0500

    Clase Gasto

--- ControlDeGastos/src/logica/Gasto.java
+++ ControlDeGastos/src/logica/Gasto.java
@@ -1,12 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package logica;
 
+import datos.Conexion;
 import datos.GastoDAO;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 /**
@@ -42,14 +44,64 @@ public class Gasto implements GastoDAO{
         this.descripcion = descripcion;
     }
 
+<<<<<<< Updated upstream
     @Override
     public ObservableList<Gasto> consultarGasto(LocalDate fecha) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 
+=======
+    public ObservableList<Gasto> consultarGasto(Date fecha) {
+        Connection conexion = null;
+        PreparedStatement sentencia = null;
+        ResultSet rs = null;
+        Gasto gastoResultado;
+        ObservableList<Gasto> listaGastos = FXCollections.observableArrayList();
+        
+        try {
+            conexion = new Conexion().connection();
+            String consulta = "SELECT gasto, descripcion FROM Gasto WHERE fecha = ?";
+            sentencia = conexion.prepareStatement(consulta);
+            sentencia.setDate(1, fecha);
+            rs = sentencia.executeQuery();
+            
+            while(rs.next()) {
+                gastoResultado = new Gasto();
+                gastoResultado.setGasto(rs.getDouble("gasto"));
+                gastoResultado.setDescripcion(rs.getString("descripcion"));
+                listaGastos.add(gastoResultado);
+            }
+        } catch (SQLException ex) {
+            //ERROR
+        } finally {
+            //Error
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    //Error
+                }
+            }
+            if (sentencia != null) {
+                try {
+                    sentencia.close();
+                } catch (SQLException ex) {
+                    //Error
+                }
+            }
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (SQLException ex) {
+                    //Error
+                }
+            }
+        }
+        return listaGastos;
+>>>>>>> Stashed changes
     }
-
-    @Override
+    
     public boolean agregarGasto(double Gasto, LocalDate fecha, String descripcion) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //POPIS
+        return false;
     }
 }
